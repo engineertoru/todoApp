8876a7262206dd47e1f738a71135581c599340b2
engineertoru <tanabata0577@gmail.com>
2019-11-29 22:31:06 +0900

    first commit

---
+++ todoApp/Todo.swift
@@ -0,0 +1,13 @@
+//
+//  Todo.swift
+//  todoApp
+//
+//  Created by toru murayama  on 2019/11/29.
+//  Copyright © 2019 toru murayama . All rights reserved.
+//
+
+import UIKit
+
+class Todo: Object {
+
+}
